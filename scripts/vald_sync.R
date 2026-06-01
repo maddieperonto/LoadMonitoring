08af006522dde0288d37dcf1a05d1a4f5e772026
Maddie Peronto <maddieperonto@Maddies-MacBook-Air.local>
2026-06-01 04:42:37 -0400

    Fix VALD audience to vald-api-external

--- scripts/vald_sync.R
+++ scripts/vald_sync.R
@@ -27,7 +27,7 @@ VALD_TENANT_ID       <- Sys.getenv("VALD_DUENDE_ID")
 SUPABASE_URL         <- Sys.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_KEY <- Sys.getenv("SUPABASE_SERVICE_KEY")
 
-VALD_TOKEN_URL <- "https://security.valdperformance.com/connect/token"
+VALD_TOKEN_URL <- "https://auth.prd.vald.com/oauth/token"
 VALD_API_BASE        <- "https://api.vald.com"
 
 # How far back to look for new tests (days)
@@ -55,7 +55,8 @@ token_resp <- request(VALD_TOKEN_URL) |>
   req_body_form(
     grant_type    = "client_credentials",
     client_id     = VALD_CLIENT_ID,
-    client_secret = VALD_CLIENT_SECRET
+    client_secret = VALD_CLIENT_SECRET,
+    audience      = "vald-api-external"
   ) |>
   req_error(is_error = function(resp) FALSE) |>
   req_perform()
